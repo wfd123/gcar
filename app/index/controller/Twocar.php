028ef68d871f2693303850a92fa942f15ef5913b
qian-php <1371802215@qq.com>
2018-11-13 14:54:27 +0800

    Update app/index/controller/Twocar.php
    前台二手车

--- app/index/controller/Twocar.php
+++ app/index/controller/Twocar.php
@@ -74,6 +74,7 @@ class Twocar extends Common
         //关于二手车
         $er_car = $this->er_car($city_id);//
 
+
         // $min_time = $this->lots_two_cars(7);//二手车 时间最新（默认）
 
         $min_price = $this->lots_two_cars(1);//二手 价格最低
@@ -87,6 +88,15 @@ class Twocar extends Common
         $ABC = $this->app_brand_ios();//A b c  按车型排序
 
         //dump($er_car);die;
+
+
+        $cat = Db::table('rele_car')->where("status=1 and up_under=1 and city_id=$city_id")->paginate(8);
+        $items = $cat->items();
+        foreach ($cat as $k => $v){
+            $items[$k]['news_price'] = Db::table('new_car')->where(['id' => $v['news_price']])->find();
+        }
+        $this->assign('items',$items);
+        $this->assign('cat',$cat);
 
         $this->assign('brand',$brand);
         $this->assign('price',$price);
@@ -105,7 +115,7 @@ class Twocar extends Common
 
         return $this->fetch();
     }
-    
+
     /*
      * 车型对比
      */
