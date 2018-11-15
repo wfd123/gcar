109263812396c887256fe8903736dd591aef6518
Administrator <17638332352@163.com>
2018-11-15 11:33:55 +0800

    更新新车列表

--- app/home.php
+++ app/home.php
@@ -16,7 +16,7 @@ return[
         // 服务器地址
         'hostname'        => '127.0.0.1',
         // 数据库名
-        'database'        => 'g_car4',
+        'database'        => 'shop',
         // 用户名
         'username'        => 'root',
         // 密码
